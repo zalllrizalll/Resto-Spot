18d8a174e7221925a8373b241cb517e302b9d248
Rizal Pratama <zalllrizalll012@gmail.com>
2025-02-24 20:31:50 +0700

    fix: lunch hour time

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -52,7 +52,7 @@ class NotificationService {
   tz.TZDateTime _nextInstanceOfElevenAM() {
     final tz.TZDateTime now = tz.TZDateTime.now(tz.local);
     tz.TZDateTime scheduledDate =
-        tz.TZDateTime(tz.local, now.year, now.month, now.day, 20, 03);
+        tz.TZDateTime(tz.local, now.year, now.month, now.day, 11);
     if (scheduledDate.isBefore(now)) {
       scheduledDate = scheduledDate.add(const Duration(days: 1));
     }
